2145bd45b80c67ddf8d5a5403d056e124efe8fab
sagar-pardhi <sgrpardhi007@gmail.com>
2022-03-20 12:40:26 +0530

    added detail view for houses

--- components/House.js
+++ components/House.js
@@ -1,13 +1,15 @@
-import React from "react";
+import Link from "next/link";
 
 export default function House({ house }) {
   return (
-    <div>
-      <img src={house.picture} width="100%" alt="House picture" />
-      <p>
-        {house.type} - {house.town}
-      </p>
-      <p>{house.title}</p>
-    </div>
+    <Link href="/houses/[id]" as={`/houses/${house.id}`}>
+      <a>
+        <img src={house.picture} width="100%" alt="House picture" />
+        <p>
+          {house.type} - {house.town}
+        </p>
+        <p>{house.title}</p>
+      </a>
+    </Link>
   );
 }
